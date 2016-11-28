00f3e6c22b1be13da2957f079f55c7876737dc92
Viterbi1992 <wangchaonihao@163.com>
2016-11-28 13:30:16 +0800

    finish cache

---
+++ learningAVPlaying/learningAVPlaying/customAVPlaying/playView/progressSlideView.h
@@ -0,0 +1,13 @@
+//
+//  progressSlideView.h
+//  learningAVPlaying
+//
+//  Created by 掌上先机 on 16/11/26.
+//  Copyright © 2016年 wangchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface progressSlideView : UIView
+
+@end
---
+++ learningAVPlaying/learningAVPlaying/customAVPlaying/playView/progressSlideView.m
@@ -0,0 +1,21 @@
+//
+//  progressSlideView.m
+//  learningAVPlaying
+//
+//  Created by 掌上先机 on 16/11/26.
+//  Copyright © 2016年 wangchao. All rights reserved.
+//
+
+#import "progressSlideView.h"
+
+@implementation progressSlideView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
